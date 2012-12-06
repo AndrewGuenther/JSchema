7fc0a41093423e3d6cad57b59a60b85f816066a2
Marc Zych <marczych@gmail.com>
2012-12-06 09:03:54 +0000

    Save migration to migrations collection

--- migrate.js
+++ migrate.js
@@ -10,21 +10,24 @@ var migrate = function(migration) {
       {$set: migration.update}
    );
 
-   // No update function supplied, we're done here.
-   if (!migrate.updateFunction) {
-      return;
+   if (migration.updateFunction) {
+      // Update all existing documents.
+      col.find().forEach(function(doc) {
+         var updatedVal = migration.updateFunction(doc);
+
+         // Only save the updated document if it is valid.
+         if (updatedVal) {
+            db.eval("v_insert('" + migration.collection +
+             "', " + tojson(updatedVal) + ", '" + migration.dataType + "')");
+         }
+      });
    }
 
-   // Update all existing documents.
-   col.find().forEach(function(doc) {
-      var updatedVal = migration.updateFunction(doc);
+   // Add a timestamp.
+   migration.timestamp = new Date().getTime();
 
-      // Only save the updated document if it is valid.
-      if (updatedVal) {
-         db.eval("v_insert('" + migration.collection +
-          "', " + tojson(updatedVal) + ", '" + migration.dataType + "')");
-      }
-   });
+   // Save the migration in the migrations collection.
+   db.migrations.save(migration);
 }
 
 db.system.js.save({_id: 'migrate', value: migrate});
